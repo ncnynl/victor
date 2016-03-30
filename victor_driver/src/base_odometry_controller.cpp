248230c98470f83f9c4fc7e0998d194ae1ed2eef
qjones81 <qjones@c4solutions.net>
2016-03-29 23:58:12 -0500

    Add GitIgnore.  Add Turn to Odom Navigation

--- victor_driver/src/base_odometry_controller.cpp
+++ victor_driver/src/base_odometry_controller.cpp
@@ -80,6 +80,78 @@ public:
       return true;
     return false;
   }
+  bool Turn(double radians)
+  {
+    
+    bool clockwise = true;
+    if(radians < 0)
+      clockwise = false;
+    
+    // Map Zero to 2*PI ( 360 deg )
+    while(radians < 0) radians += 2*M_PI;
+    while(radians > 2*M_PI) radians -= 2*M_PI;
+
+    //wait for the listener to get the first message
+    _listener.waitForTransform("base_link", "odom", 
+                               ros::Time(0), ros::Duration(1.0));
+    
+    //we will record transforms here
+    tf::StampedTransform start_transform;
+    tf::StampedTransform current_transform;
+
+    //record the starting transform from the odometry to the base frame
+    _listener.lookupTransform("base_link", "odom", 
+                              ros::Time(0), start_transform);
+    
+    //we will be sending commands of type "twist"
+    geometry_msgs::Twist base_cmd;
+    //the command will be to turn at 0.75 rad/s
+    base_cmd.linear.x = base_cmd.linear.y = 0.0;
+    base_cmd.angular.z = 0.75;
+    if (clockwise) 
+      base_cmd.angular.z = -base_cmd.angular.z;
+    
+    //the axis we want to be rotating by
+    tf::Vector3 desired_turn_axis(0,0,1);
+    if (!clockwise) 
+      desired_turn_axis = -desired_turn_axis;
+    
+    ros::Rate rate(10.0);
+    bool done = false;
+    while (!done && _nh.ok())
+    {
+      //send the drive command
+      _cmd_vel_pub.publish(base_cmd);
+      rate.sleep();
+      //get the current transform
+      try
+      {
+        _listener.lookupTransform("base_link", "odom", 
+                                  ros::Time(0), current_transform);
+      }
+      catch (tf::TransformException ex)
+      {
+        ROS_ERROR("%s",ex.what());
+        break;
+      }
+      tf::Transform relative_transform = 
+        start_transform.inverse() * current_transform;
+      tf::Vector3 actual_turn_axis = 
+        relative_transform.getRotation().getAxis();
+      double angle_turned = relative_transform.getRotation().getAngle();
+      if ( fabs(angle_turned) < 1.0e-2) 
+	continue;
+
+      if ( actual_turn_axis.dot( desired_turn_axis ) < 0 ) 
+        angle_turned = 2 * M_PI - angle_turned;
+
+      if (angle_turned > radians) 
+	done = true;
+    }
+    if (done) 
+      return true;
+    return false;
+  }
 };
 
 int main(int argc, char** argv)
@@ -90,4 +162,7 @@ int main(int argc, char** argv)
 
   BaseOdometryDriveController driver(nh);
   driver.DriveForward(5.0);
+  driver.Turn(M_PI);
+  driver.DriveForward(5.0);
+  
 }
